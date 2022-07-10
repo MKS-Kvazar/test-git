86636dec4fd2d836e51ee4911092ae09bfa80bb7
Kostiantyn <mksgps@gmail.com>
2022-07-10 20:51:56 +0300

    Create branch 02

--- src/mks/Main.java
+++ src/mks/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("create branch 01");
+        System.out.println("create branch 02");
         // write your code here
     }
 }
