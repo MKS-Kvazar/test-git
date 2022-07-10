77d26252c0a95e937fe9038feca9ad36b8574797
Kostiantyn <mksgps@gmail.com>
2022-07-10 20:52:42 +0300

    Change-01 branch 02

--- src/mks/Main.java
+++ src/mks/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("create branch 01");
         System.out.println("create branch 02");
+        System.out.println("change-1 branch 02");
+        System.out.println("change-2 branch 02");
+        System.out.println("change-3 branch 02");
         // write your code here
     }
 }
